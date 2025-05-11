4b413fc4fa86ec11954538b9e81702ad75e55457
Nathan Samuell <nathansamuell@duck.com>
2025-05-10 22:20:18 -0500

    added transparent background\!

--- init.lua
+++ init.lua
@@ -231,6 +231,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'dylanaraps/wal', -- NATHAN ADDED: updates color scheme based off of wal colors
+  'xiyaowong/transparent.nvim', -- NATHAN ADDED: transparent background!
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
